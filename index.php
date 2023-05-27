0a0efb441c9bc008dc028d498456d3278fefeb7c
SUSHEELA568 <134774741+SUSHEELA568@users.noreply.github.com>
2023-05-27 15:30:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,615 @@
+﻿<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Explore World</title>
+	
+<!-- [ FONT-AWESOME ICON ]       
+=========================================================================================================================-->
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+<!-- [ PLUGIN STYLESHEET ]
+=========================================================================================================================-->
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+=========================================================================================================================-->
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+=========================================================================================================================-->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/green.css">
+
+</head>
+<body >
+<!-- [ LOADERs ]
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span></span>
+      
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html"><span class="themecolor">E</span>xplore <span class="themecolor">W</span>orld</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>HOME</h3></a></li>
+            
+<li><a href="#one" class="page-scroll"><h3>ABOUT</h3></a></li>
+                                                                 
+<li><a href="#two" class="page-scroll"><h3>CONTACT</h3></a></li>
+            
+<li><a href="#three" class="page-scroll"><h3>SERVICES</h3></a></li>
+
+<li><a href="admin/" class="page-scroll"><h3>LOGIN</h3></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements="Online Ticket Reservation, Online Ticket Reservation, Online Ticket Reservation"></span></h1>
+ 
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Search..Book..Go..Enjoy..., Search..Book..Go..Enjoy..., Search..Book..Go..Enjoy..."></span></h1>
+       
+<p class="main-heading-text">Keep exploring,you will discover new path,<br/>It's time for a new adventure trip.</p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Book Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+<section class="main-heading" id="home">
+       
+<div class="overlay" >
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="rose">WELCOME TO EXPLORE WORLD SITE FOR ONLINE TICKET RESERVATION</span></h1>
+        
+<p class="main-heading-text">Your Travel Is Easy And Safe Here,
+<br/>In your Busy and Hectic Schedule,we're here with you for ensuring your safe and feasible travel experience!!</p>
+        
+<div class="btn-bar">
+          
+<a href="#" class="btn btn-custom theme_background_color">Get Started</a>
+          
+<a href="#" class="btn btn-custom-outline">Contact Us</a>
+        
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+    
+ 
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [ABOUT US]
+ 
+============================================================================================================================-->
+ 
+<section class="aboutus white-background black" id="one" style="background-color: maroon;">
+     
+<div class="container">
+         
+<div class="row">
+             
+<div class="col-md-12 text-center black">
+                 
+<h3 class="title">ABOUT <span class="themecolor">US</span></h3>
+            
+<p class="a-slog">Book your tickets online with Explore World and check out fare,availability and confirmation status.</p> 
+             
+</div>
+         
+</div>
+         
+<div class="gap">
+             
+         
+</div>
+         
+         
+<div class="row about-box">
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-newspaper-o"></i>
+              
+<h4 class="white">EASILY FEASIBLE</h4>
+              
+<p class="black">We Ensure your Travel with us under feasible fare.</p>
+            
+</div> <!-- / margin -->
+          
+</div> <!-- /col -->
+          
+<div class="col-sm-4 about-line text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-diamond"></i>
+              
+<h4 class="white">FULLY RESPONSIVE</h4>
+              
+<p class="black">We're responsive for your happy,safe and secure journey with us.</p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-area-chart"></i>
+              
+<h4 class="white">DISCOUNT</h4>
+              
+<p class="black">10% discount was given to students,20 % discount was given to senior citizens and for children above 3 years old was 
+  given 30 % discount for traveling in all classes,</br>
+  The minimum age limit for availing free ticket was between 0 to 3 years old..</p>
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+        
+</div> <!-- /row -->
+         
+</div>
+ </section>
+ 
+<!-- [/ABOUTUS]
+ 
+============================================================================================================================-->
+ 
+<!-- [INSPIRATION]
+ 
+============================================================================================================================-->
+ 
+<section class="inspiration" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>Are You Ready To <span class="themecolor">Explore And Enjoy With Us?</span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2>For Further More Details</h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline"><span>Contact : </span>susheela@gmail.com</a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+<!-- [/INSPIRATION]
+ 
+
+============================================================================================================================-->
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<section class="services white-background black" id="seven" style="background-color:lightpink;">
+     
+ <div class="container">
+        
+<div class="row text-center">
+          
+<div class="col-md-12">
+              
+<h3 class="title">We Also <span class="themecolor">Provide</span></h3>
+<h4 class="title">Have you not found the right one?<br>
+Find a service suitable for you here.</h4>
+<p class="a-slog">Apart from train booking,we also have the below services..Hope you Enjoy our facilities!!</p>
+          
+</div> <!-- /col -->
+        
+</div> <!-- /row -->
+        
+<div class="gap"></div>
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-map-marker"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Bus Ticket Booking</h4>
+                
+<p>We provide safe and happy Journey.</p>
+   
+           </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-diamond"></i>
+             
+ <div class="nim-service-detail">
+              
+ <h4>Hotel Booking</h4>
+              
+ <p>We provide a tasty and hygine food, safe,clean and luxurious rooms of your budget.</p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-tablet"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Movie Ticket Booking</h4>
+                
+<p>We also provide Movie Ticket Booking, so that you can have fun and enjoy the movies with your family and friends.</p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->       
+        
+</div> <!-- end row -->
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+<i class="fa fa-magic"></i>
+             
+<div class="nim-service-detail">
+               
+<h4>Online Food Delivery</h4>
+                
+<p>we also provide online food ordering facility ,To refresh your taste buds where you can place a order from top hotels in and around the city ypur travelling!! </p>
+          
+    </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->                    
+       
+      
+ <div class="col-sm-4">
+            
+ <div class="nim-service margin-bottom">
+                                   
+  <i class="fa fa-pencil-square"></i>
+                                   
+  <div class="nim-service-detail">
+                                     
+  <h4>Holiday Package</h4>
+                                      
+  <p>we also provide Holiday Package service,To enjoy the weekend with your family and friends with affordable price range tours!! </p>
+                                
+     </div> <!-- /nim-service-detail -->
+                                  
+</div> <!-- /nim-service margin-bottom -->
+                               
+  </div> <!-- /col -->                    
+                      
+<div class="col-sm-4">
+                      
+<div class="nim-service margin-bottom">
+                                             
+  <i class="fa fa-rocket"></i>
+                                             
+    <div class="nim-service-detail">
+                                               
+    <h4>Flight Ticket Booking</h4>
+                                                
+    <p>we also provide Flight Ticket Booking service,To enjoy vacation around the world. </p>
+                                          
+      </div> <!-- /nim-service-detail -->
+                                            
+</div> <!-- /nim-service margin-bottom -->
+                                         
+  </div> <!-- /col -->                    
+                                       
+  </div> <!-- end row -->
+          
+</div>  <!-- container -->
+    
+</section>
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================--> 
+ 
+<!-- [CONTACT]
+ 
+============================================================================================================================-->
+ 
+<!--sub-form-->
+<section class="sub-form text-center" id="eight">
+  
+<div class="container">
+    <div class="col-md-12">
+        
+<h3 class="title">Feed<span class="themecolor">B</span>ack</h3>
+            
+<p class="lead">your feedback is very important to improve our facility..</p>
+    
+</div> 
+    
+<div class="row">
+        
+<div class="col-md-3 col-sm-3"></div>
+      
+<div class="col-md-6 center-block col-sm-6 ">
+        
+<form id="mc-form">
+          
+<div class="input-group">
+            
+<input type="email" class="form-control" placeholder="Email Address" required id="mc-email">
+           
+<span class="input-group-btn">
+            
+<button type="submit" class="btn btn-default">SUBMIT<i class="fa fa-envelope"></i> </button>
+            
+</span> </div>
+          
+<label for="mc-email" id="mc-notification"></label>
+       
+ </form>
+      
+</div>
+   
+ </div>
+  
+</div>
+
+</section>
+
+<!--sub-form end--> 
+
+<!-- [/CONTACT]
+============================================================================================================================-->
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      
+</div>
+      
+<div class="col-md-4"> <small>&copy; Copyrights reserved @2023.</small></div>
+      
+<div class="col-md-4"> 
+        
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+<!-- [/FOOTER]
+============================================================================================================================-->
+</div>
+ 
+<!-- [ /WRAPPER ]
+=============================================================================================================================-->
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+</html>
